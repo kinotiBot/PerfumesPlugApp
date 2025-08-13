d982dbd532eadddc513e047f737e453084c54ccd
kinotiBot <125564718+kinotiBot@users.noreply.github.com>
2025-08-13 18:45:16 +0200

    Update settings.py

--- perfumes_project/settings.py
+++ perfumes_project/settings.py
@@ -105,7 +105,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.environ.get('RAILWAY_VOLUME_MOUNT_PATH', os.path.join(BASE_DIR, 'media'))
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
