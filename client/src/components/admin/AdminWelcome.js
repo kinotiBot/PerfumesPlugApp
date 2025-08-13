2dad37b58e1d7e965351d85a10ef84b7a0716fff
kinotiBot <125564718+kinotiBot@users.noreply.github.com>
2025-08-13 14:54:25 +0200

    Release v1.009

--- client/src/components/admin/AdminWelcome.js
+++ client/src/components/admin/AdminWelcome.js
@@ -16,8 +16,6 @@ import {
   Dashboard,
   Inventory,
   ShoppingCart,
-  People,
-  Settings,
 } from '@mui/icons-material';
 
 const AdminWelcome = () => {
--- client/src/components/checkout/GuestCheckoutForm.js
+++ client/src/components/checkout/GuestCheckoutForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import {
-  Box,
   TextField,
   Typography,
   Paper,
